6d6ee651d93ac88fcfd2c1202d46b1635367008e
Nguyen Huynh Minh Tri <triminh0502@gmail.com>
2024-07-02 15:47:44 +0700

    Create Window

---
+++ WPF-GoodsExchangeFU/WPF-GoodsExchangeFUGUI/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WPF_GoodsExchangeFUGUI
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
